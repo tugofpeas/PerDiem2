c37e52e5e79640174aa54572c1a9931928d699f4
Alysia Petti <alysiapetti@gmail.com>
2017-07-12 10:12:22 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,28 +3,28 @@
  *  Date: 2017-07-06
  *  Description: A more simplified financial calculator to compute allocated amount for meals. 
  */
-//  http://jsfiddle.net/karalamalar/8e92d/
 
+
+//VARIABLE FOR CALCULATIONS
 //Meals Per day
 function mealsPerDay(perDiemRate){
 	return perDiemRate - 5;
 }
 
+//VARIABLE FOR CALCULATIONS
 //Meals percentage of 75%
 function mealPercentage(mealsPerDay, perDiemRate) {
 	return mealsPerDay(perDiemRate)*0.75;
 }
 
+//VARIABLE FOR CALCULATIONS
 //Meals percentage of 35%
 function twoMealsPercentage(mealsPerDay, perDiemRate){
 	return mealsPerDay(perDiemRate)*0.35;
 }
 
 
-//Total for less than 20 hours 1st & last day
-function twentyMeal(mealType, lessThanTwenty, mealsPerDay, perDiemRate){
-		return mealsPercentage(perDiemRate)+5;
-}
+//RESULTS
 
 //Total for one meal during less than 10 hours 1st & last day
 function oneTenMeal(mealType, lessThanTen, numbMeals, mealsPercentage, perDiemRate){
@@ -48,23 +48,26 @@ function twoConferenceMeal(mealType, conference, numbMeals, twoMealsPercentage, 
 }
 
 
-function totalAmount(perDiemRate, mealType, lessThanTen, lessThanTwenty, conference, numbMeals){
-	if(mealType == lessThanTwenty && numbMeals == 1 || 2) {
-		return twentyMeal(mealType, lessThanTwenty, mealsPerDay, perDiemRate);
-	} else {
-		if(mealType == lessThanTen && numbMeals == 1) {
-			return oneTenMeal(mealType, lessThanTen, numbMeals, mealsPercentage, perDiemRate);
-		} else {
-			if(mealType == conference && numbMeals == 1) {
-				return oneConferenceMeal(mealType, conference, numbMeals, mealsPercentage, perDiemRate);
-			} else {
-				if(mealType == conference && numbMeals ==2){
-					return twoConferenceMeal(mealType, conference, numbMeals, twoMealsPercentage, perDiemRate);
-				}
-			}
-		}
-	}
-};
+//switch case for selecting meal types 
+function totalAmount(mealType) {
+  switch(mealType) {
+    case "First & Last Day < 10 hrs.":
+      if (numbMeals == 1) {
+      	return oneTenMeal(mealType, lessThanTen, numbMeals, mealsPercentage, perDiemRate);
+      } else if (numbMeals == 2) {
+      	return twoTenMeals(mealsPercentage, perDiemRate);
+      }
+      break;
+    case "Conference Meals":
+      if (numbMeals == 1) {
+      	return oneConferenceMeal(mealType, conference, numbMeals, mealsPercentage, perDiemRate);
+      } else if (numbMeals == 2) {
+      	return twoConferenceMeal(mealType, conference, numbMeals, twoMealsPercentage, perDiemRate);
+      }
+      break;
+  }
+}
+
 
 
 
@@ -75,11 +78,7 @@ function getRates()
 {
 	var perDiemRate = document.getElementsByName("PerDiemRate")[0].value;
 	var mealType = document.getElementsByName("MealType")[0].value;
-	var lessThanTwenty = document.getElementsByID("LessThanTwenty")[0].innerHTML;
-	var lessThanTen = document.getElementsByID("LessThanTen")[0].value;
-	var conference = document.getElementsByID("Conference")[0].value;
 	var numbMeals = document.getElementsByName("NumbMeals")[0].value;
-	//define which function above will show result in TotalAmount...
 	document.getElementsByName("TotalAmount")[0].value = totalAmount();
 	
 	
@@ -88,11 +87,9 @@ function getRates()
 //reset function for all values
 function resetFields()
 {
-	document.getElementsByName("PerDiemRate")[0].value = 0.00;
-	document.getElementsByName("MealType")[0].value = 0.00;
-	document.getElementsByName("LessThanTwenty")[0].value = 0.00;
-	document.getElementsByName("Conference")[0].value = 0.00;
-	document.getElementsByName("NumbMeals")[0].value = 0.00;
-	document.getElementsByName("TotalAmount")[0].value = 0.00;
+	document.getElementsByName("PerDiemRate")[0].value = 0.00.toFixed(2);
+	document.getElementsByName("MealType")[0].value = "First & Last Day < 10 hrs.";
+	document.getElementsByName("NumbMeals")[0].value = 1;
+	document.getElementsByName("TotalAmount")[0].value = 0.00.toFixed(2);
 	
 }
